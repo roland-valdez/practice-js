86269c376e7305dc43b6fbeff062bfece93a1f30
Roland Valdez <valdez.roland.23@gmail.com>
2021-02-13 21:33:22 -0600

    added code up to excercies 8

--- W3-exercises/js/W3-loops.js
+++ W3-exercises/js/W3-loops.js
@@ -32,15 +32,58 @@ sign(-1, -1, 3)
 // 3. Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result. Go to the editor
 // Sample numbers : 0, -1, 4
 // Output : 4, 0, -1
+function randomsort(number1, number2, number3){
+var first = 0, second = 0, third = 0;
+    if (number1 > number2 && number1 > number3) {// checks to see highest value between all numbers
+        first = number1;
+    }else if (number2 > number1 && number2 > number3){
+        first = number2;
+    }else{
+        first = number3;
+    }
 
+    if (first !== number1 && number1 < number2 && number1 < number3) {//checks for lowest value
+        third = number1;
+    }else if (first !== number2 && number2 < number1 && number2 < number3){
+        third = number2;
+    }else {
+        third = number3;
+    }
+
+    if (first !== number1 && third !== number1){
+        second = number1;
+
+    }else if (first !== number2 && third !== number2){
+        second = number2;
+    }else {
+        second = number3;
+    }
+
+    return alert("These are the number you entered sorted out: " + first +", " + second + ", " + third);
+}
+randomsort(0, -1, 4);
 
 
 
 // 4. Write a JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the result. Go to the editor
 // Sample numbers : -5, -2, -6, 0, -1
 // Output : 0
-
-
+function largestOfFive(num1, num2, num3, num4, num5){
+    var largest = 0;
+    if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5){
+        largest = num1;
+    }else if (num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5){
+        largest = num2;
+    }else if(num3 > num2 && num3 > num1 && num3 > num4 && num3 > num5){
+        largest = num3;
+    }else if (num4 > num2 && num4 > num3 && num4 > num1 && num4 > num5){
+        largest = num4;
+    }else {
+        largest = num5;
+    }
+    return alert(largest);
+}
+largestOfFive(-5, -2, -6, 0,-1)
 
 
 // 5. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen. Go to the editor
@@ -49,8 +92,16 @@ sign(-1, -1, 3)
 // "1 is odd"
 // "2 is even"
 // ----------
-
-
+function loopTo15(){
+    for (var i = 0; i <= 15; i++) {
+        if (i % 2 == 0) {
+            alert(i + " is even");
+        } else {
+            alert(i + " is odd");
+        }
+    }
+}
+loopTo15();
 
 
 // 6. Write a JavaScript program which compute, the average marks of the following students Then, this average is used to determine the corresponding grade. Go to the editor
@@ -69,20 +120,68 @@ sign(-1, -1, 3)
 // <80	C
 // <90	B
 // <100	A
-
-
-
+function grades() {
+    var students = ["David", "Vinoth", "Divya", "Ishitha", "Thomas"];
+    var grades = [80, 77, 88, 95, 68]
+    for (var i = 0; i < grades.length; i++){
+        if (grades[i] <60){
+           alert(students[i] + " you got an F.")
+        }else if (grades[i] < 70){
+            alert(students[i] + " you got an D.")
+        }else if (grades[i] < 80) {
+            alert(students[i] + " you got an C.")
+        }else if (grades[i] < 90) {
+            alert(students[i] + " you got an B.")
+        }else
+            alert(students[i] + " you got an A.")
+    }
+}
+grades();
 
 // 7. Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz". Go to the editor
-
-
-
+function oneTo100() {
+    for (var i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+oneTo100();
 
 // 8. According to Wikipedia a happy number is defined by the following process :
 //     "Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers, while those that do not end in 1 are unhappy numbers (or sad numbers)".
 //         Write a JavaScript program to find and print the first 5 happy numbers. Go to the editor
-
-
+function happyNumbers(){
+    var k = 0;
+    for (var i = 0; i < 100; i++) {
+        var num = i;
+        for (var j = 0; j < 1000; j++){
+            if (num < 10) {
+                num = Math.pow(num, 2);
+            }else {
+                var numString = num.toString();
+                var num1 = numString[0];
+                var num2 = numString[1];
+                num = Math.pow(num1, 2) + Math.pow(num2, 2);
+            }
+            if (num === 1) {
+                console.log("Happy Number:" + i);
+                k++;
+                break;
+            }else if (j >= 99){
+                break;
+            }
+        }
+        if(k === 5){break;}
+    }
+}
+happyNumbers();
 
 
 // 9. Write a JavaScript program to find the armstrong numbers of 3 digits. Go to the editor
