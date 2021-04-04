d0ff8ec2787257bc6e2ed406b7039818120fc51f
Roland Valdez <valdez.roland.23@gmail.com>
2021-04-04 15:55:37 -0500

    added some solutions to W3-array excercies

--- W3-exercises/js/W3-arrays.js
+++ W3-exercises/js/W3-arrays.js
@@ -13,10 +13,16 @@ console.log(is_array([1, 2, 4, 0]));
 
 // 2. Write a JavaScript function to clone an array. Go to the editor
 // Test Data :
-//     console.log(array_Clone([1, 2, 4, 0]));
+// console.log(array_Clone([1, 2, 4, 0]));
 // console.log(array_Clone([1, 2, [4, 0]]));
 // [1, 2, 4, 0]
 // [1, 2, [4, 0]]
+function array_Clone(array){
+    var newArray = array;
+    return newArray;
+}
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
 
 // 3. Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array. Go to the editor
 // Test Data :
@@ -31,6 +37,27 @@ console.log(is_array([1, 2, 4, 0]));
 // [7, 9, 0]
 // [7, 9, 0, -2]
 // []
+function first(array, num){
+    if (num == undefined){
+        return array[0];
+    }
+    else if (num <= 0){
+        return [];
+    }
+    else if (num > array.length){
+        return array;
+    }
+    else {
+        var slice = array.slice(0, num);
+       return slice;
+    }
+}
+
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
 
 // 4. Write a JavaScript function to get the last element of an array. Passing a parameter 'n' will return the last 'n' elements of the array. Go to the editor
 // Test Data :
